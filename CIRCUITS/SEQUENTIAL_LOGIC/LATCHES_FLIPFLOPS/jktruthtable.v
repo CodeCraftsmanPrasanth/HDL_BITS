bca661a7df37f8255f3b7a2c3cb9af1666ad2dc2
Prasanth <162542956+CodeCraftsmanPrasanth@users.noreply.github.com>
2025-08-26 15:23:28 +0530

    Create jktruthtable.v

---
+++ CIRCUITS/SEQUENTIAL_LOGIC/LATCHES_FLIPFLOPS/jktruthtable.v
@@ -0,0 +1,11 @@
+module top_module (
+    input clk,
+    input j,
+    input k,
+    output Q);
+    wire d;
+    assign d = (j & ~Q) | (~k & Q);
+    always @(posedge clk) begin
+        Q <= d;
+    end   
+endmodule

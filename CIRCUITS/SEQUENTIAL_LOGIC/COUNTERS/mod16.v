ce2f15dcbfe8262c1f6d5e498fb630b71a30958a
Prasanth <162542956+CodeCraftsmanPrasanth@users.noreply.github.com>
2025-08-26 17:59:32 +0530

    Create mod16.v

---
+++ CIRCUITS/SEQUENTIAL_LOGIC/COUNTERS/mod16.v
@@ -0,0 +1,9 @@
+module top_module (
+    input clk,
+    input reset,      // Synchronous active-high reset
+    output [3:0] q);
+    always @(posedge clk) begin
+        if ((reset) | (q==4'hf)) q<=0;
+        else q<=q+1'h1;
+    end
+endmodule

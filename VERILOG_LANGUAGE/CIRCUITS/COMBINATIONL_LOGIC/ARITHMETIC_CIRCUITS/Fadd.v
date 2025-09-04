9eb1c7d84ef975f54bf131636de6e9f26fbcc6b6
Prasanth <162542956+CodeCraftsmanPrasanth@users.noreply.github.com>
2025-09-04 14:54:46 +0530

    Delete VERILOG_LANGUAGE/CIRCUITS directory

--- VERILOG_LANGUAGE/CIRCUITS/COMBINATIONL_LOGIC/ARITHMETIC_CIRCUITS/Fadd.v
+++
@@ -1,5 +0,0 @@
-module top_module( 
-    input a, b, cin,
-    output cout, sum );
-    assign {cout,sum}={{a&b|a&cin|b&cin},a^b^cin};
-endmodule
--- VERILOG_LANGUAGE/CIRCUITS/COMBINATIONL_LOGIC/BASIC_GATES/m2014 q4f.v
+++
@@ -1,6 +0,0 @@
-module top_module (
-    input in1,
-    input in2,
-    output out);
-	assign out=in&~in2;
-endmodule
